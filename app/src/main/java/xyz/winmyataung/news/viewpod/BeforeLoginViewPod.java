da00a448e29cdd472901ce8335b9c31a45b052ea
DESKTOP-49KO4JJ\DELL <winmyataung.page15@gmail.com>
2018-01-20 12:39:25 +0630

    before login

---
+++ app/src/main/java/xyz/winmyataung/news/viewpod/BeforeLoginViewPod.java
@@ -0,0 +1,27 @@
+package xyz.winmyataung.news.viewpod;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by DELL on 1/20/2018.
+ */
+
+public class BeforeLoginViewPod extends RelativeLayout {
+    public BeforeLoginViewPod(Context context) {
+        super(context);
+    }
+
+    public BeforeLoginViewPod(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BeforeLoginViewPod(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BeforeLoginViewPod(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
